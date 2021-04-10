3b0838494b4fdaf1922e1a4c865d8fb4e0545d49
Aleksandr Vdovenko <vdovenkoal@gmail.com>
2021-04-11 02:23:38 +0400

    - refactor fibonacci cards - change client logic for socket.io

--- src/app/components/poker/poker.component.ts
+++ src/app/components/poker/poker.component.ts
@@ -1,5 +1,9 @@
 import {Component, OnInit} from '@angular/core';
-import {FibonacciCard} from '../../models/FibonacciCard.model';
+import {FibonacciCard} from '../../models/fibonacciCard.model';
+import {ActivatedRoute} from '@angular/router';
+import {Socket} from 'socket.io-client';
+import {UserData} from '../../models/player.model';
+import {SocketService} from '../../service/socket.service';
 
 @Component({
   selector: 'app-poker',
@@ -8,72 +12,57 @@ import {FibonacciCard} from '../../models/FibonacciCard.model';
 })
 export class PokerComponent implements OnInit {
 
-  constructor() {
+  constructor(private route: ActivatedRoute,
+              private socketService: SocketService) {
   }
 
-  players: Record<string, string>[];
-  fibonacciCards: FibonacciCard[] = [
-    {
-      value: '0',
-      isSelected: false,
-    },
-    {
-      value: '1',
-      isSelected: false,
-    },
-    {
-      value: '2',
-      isSelected: false,
-    },
-    {
-      value: '3',
-      isSelected: false,
-    },
-    {
-      value: '5',
-      isSelected: false,
-    },
-    {
-      value: '13',
-      isSelected: false,
-    },
-    {
-      value: '21',
-      isSelected: false,
-    },
-    {
-      value: '34',
-      isSelected: false,
-    },
-    {
-      value: '55',
-      isSelected: false,
-    },
-    {
-      value: '89',
-      isSelected: false,
-    },
-    {
-      value: '?',
-      isSelected: false,
-    },
-  ];
+  fibonacciNumbers: string[] = ['0', '1', '2', '3', '5', '13', '21', '34', '55', '89', '?'];
+  users: Record<string, string>[];
+  fibonacciCards: FibonacciCard[];
+  socket: Socket;
+  roomId: string;
+
+  private static generateFibonacciCards(numbers: string[]): FibonacciCard[] {
+    return numbers.map(item => {
+      return {value: item, isSelected: false};
+    });
+  }
 
   ngOnInit(): void {
-    this.players = [
-      {
-        name: 'Tom',
-        card: '5',
-      },
-      {
-        name: 'Jack',
-        card: '8',
-      }
-    ];
+    this.roomId = this.route.snapshot.paramMap.get('id');
+    this.socket = this.socketService.getSocket();
+    this.socket.on('users', (users) => {
+      users.forEach((user) => {
+        user.self = user.userId === this.socket.id;
+      });
+
+      this.users = users.sort((a, b) => {
+        if (a.self) {
+          return -1;
+        }
+        if (b.self) {
+          return 1;
+        }
+        if (a.username < b.username) {
+          return -1;
+        }
+        return a.username > b.username ? 1 : 0;
+      });
+    });
+    this.socket.emit('connect-to-room', this.getPlayerData(this.roomId));
+    this.fibonacciCards = PokerComponent.generateFibonacciCards(this.fibonacciNumbers);
+  }
+
+  getPlayerData(roomId: string): UserData {
+    return {
+      username: 'Tom',
+      roomId,
+    };
   }
 
   onCardClick(selectedCard: FibonacciCard): void {
     this.fibonacciCards.forEach(card => card.isSelected = false);
     this.fibonacciCards.find(value => value.value === selectedCard.value).isSelected = true;
+    this.socket.emit('select-card', selectedCard);
   }
 }

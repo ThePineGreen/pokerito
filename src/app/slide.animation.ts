6fedba5f4765039ca83a3bcf26a3753fb5fde94a
Aleksandr Vdovenko <vdovenkoal@gmail.com>
2021-04-25 00:45:19 +0400

    animation =_=

--- src/app/slide.animation.ts
+++ src/app/slide.animation.ts
@@ -1,21 +1,19 @@
 import {animate, animation, style} from '@angular/animations';
 
 export const slideInAnimation = animation([
-    style({ opacity: 0, 'max-height': '50px', transform: 'translateY(50px)' }),
-    animate('.2s ease-in', style({ })),
-    animate('.3s ease-in', style({ opacity: 1, 'max-height': '100%', transform: 'translateY(0px)' })),
+  style({opacity: 0, transform: 'translateY(200%)'}),
+  animate('.3s ease-in', style({opacity: 1, transform: 'translateY(0%)'})),
 ]);
 
 export const slideOutAnimation = animation([
-    animate('.3s ease-in', style({ opacity: 0, height: 0, transform: 'translateY(50px)' })),
+  animate('.3s ease-in', style({transform: 'translateY(150%)'})),
 ]);
 
 export const slideInCards = animation([
-  style({ display: 'none', 'max-height': '50px', transform: 'translateY(50px)' }),
-  animate('.3s ease-in', style({display: 'block', 'max-height': '100%', transform: 'translateY(0px)'}))
+  style({transform: 'translateY(210%)'}),
+  animate('.3s ease-in', style({transform: 'translateY(0%)'}))
 ]);
 
 export const slideOutCards = animation([
-  // animate('0s linear', style({ position: 'absolute'})),
-  animate('.2s ease-in', style({ height: 0, transform: 'translateY(50px)' })),
+  animate('.3s ease-in', style({transform: 'translateY(210%)'})),
 ]);
